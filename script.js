a3e32ef6d07cc43279dbcad60114353a64bf0f94
kelteixeira <kelly.t@aluno.senai.br>
2026-07-02 18:10:41 -0300

    Continuação

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const precoIngresso = 100
 const form = document.getElementById("inscricao")
+const precoIngresso = 100;
+const form = document.getElementById("inscricao");
+let nomeSalvo = localStorage.getItem("techday_nome");
 
 function processarInscricao(nome, idade, possuiCupom) {
     let precoFinal = precoIngresso;
@@ -39,10 +42,15 @@ form.addEventListener(
         if(valorEmail.trim() === ""){
             alert("Por favor, prencha seu e-mail!")
         }
-        localStorage.setItem("techday_nome", "valorNome")
-        localStorage.setItem("techday_email", "valorEmail")
+        localStorage.setItem("techday_nome", valorNome)
+        localStorage.setItem("techday_email", valorEmail)
 
         form.style.display="none";
         const secaoinscricao = document.getElementById("secao-inscricao");
+        const mensagemSucesso = document.createElement("h3");
+        mensagemSucesso.innerText = "Inscrição confirmada! Bem Vindo(a) ao TechDay 2026!"+valorNome+"!"
+        mensagemSucesso.style.color = "green";
+        secaoInscricao.appendChild(mensagemSucesso);
+
     }
 )
